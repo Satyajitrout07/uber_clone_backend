5eaa646332633e1f39961181611bc42f7b6dbfbe
Satyajitrout07 <satya9078150307@gmail.com>
2024-12-03 10:59:47 +0530

    push

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -26,4 +26,11 @@ res.status(200).json({token,user});
 }*/
 
 
-exports.registerUser = async (req, res) => { try { const { username, password, fullname } = req.body; const user = await User.create({ username, password, fullname }); res.status(201).json(user); } catch (error) { res.status(500).json({ message: 'Error registering user', error }); } };
+exports.registerUser = async (req, res) => {
+     try {
+         const { username, password, fullname } = req.body; 
+         const user = await User.create({ username, password, fullname });
+          res.status(201).json(user);
+         } catch (error) 
+         { res.status(500).json({ message: 'Error registering user', error }); }
+         };
